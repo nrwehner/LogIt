5cc4d915083f3cfc504f7119e65b7df9e6e43839
Nick Wehner <nrwehner@gmail.com>
2020-03-31 14:07:28 -0400

    Add project files.

---
+++ LogIt.WebMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LogIt.WebMVC.MvcApplication" Language="C#" %>
---
+++ LogIt.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LogIt.WebMVC.Startup))]
+namespace LogIt.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

02133acd5b065601bb1e850acf854d4a073d2257
Nick Wehner <nrwehner@gmail.com>
2020-04-04 08:53:47 -0400

    FoodItem View Progress

---
+++ LogIt.WebMVC/Controllers/FoodItemController.cs
@@ -0,0 +1,22 @@
+﻿using LogIt.Data;
+using LogIt.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LogIt.WebMVC.Controllers
+{
+    [Authorize]
+    public class FoodItemController : Controller
+    {
+        private ApplicationDbContext _db = new ApplicationDbContext();
+        // GET: FoodItem
+        public ActionResult Index()
+        {
+            var model = new FoodItemListItem[0];
+            return View(model);
+        }
+    }
+}

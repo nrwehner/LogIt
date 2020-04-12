5220bfe00d5b46615a80afe7c1af73014a4836d8
Nick Wehner <nrwehner@gmail.com>
2020-04-12 15:12:12 -0400

    foodday view progress

---
+++ LogIt.Services/UserDataService.cs
@@ -0,0 +1,20 @@
+﻿using LogIt.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogIt.Services
+{
+    public class UserDataService
+    {
+        private ApplicationDbContext _db = new ApplicationDbContext();
+        private readonly string _userId;
+
+        public UserDataService(string userId)
+        {
+            _userId = userId;
+        }
+    }
+}
